edc83f37604ad25784c140415c6a12594f491cd5
Mark Carbonaro <mark@iovox.com>
2017-12-12 11:53:40 +1100

    Changed user agent string to Chrome 62 and added a script to start teams

--- app/lib/index.js
+++ app/lib/index.js
@@ -46,7 +46,7 @@ app.on('ready', () => {
     });
   });
 
-  window.webContents.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36');
+  window.webContents.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36');
   window.loadURL('https://teams.microsoft.com/');
 
   if (process.env.WEB_DEBUG) {
